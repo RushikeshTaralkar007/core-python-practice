346415a9ca7cd61e189cd6781c60253d37701c0d
Rushikesh Taralkar <rushikeshtaralkar07@gmail.com>
2026-02-22 20:56:00 +0530

    Update 10 python queries

--- Basics/01_variable.py
+++ Basics/01_variable.py
@@ -2,3 +2,56 @@
 
 name = "Rushikesh Taralkar"
 print("Name :", name)
+
+# Q2) Store integer and print its type
+
+num =25
+print(type(num))
+
+
+# Q3) Store float Value 
+
+price = 88.99
+print(price)
+
+# Q4) store string
+
+city = "Pune"
+print(city)
+
+# Q5) Multiple Variable Assignment
+
+a,b,c=1,2,3
+print(a,b,c)
+
+# Q6) Assign same value to multiple variables
+
+x = y = z = 100
+print(x,y,z)
+
+# Q7) Swap Two Variables
+
+a = 5
+b = 10
+a,b = b,a 
+print("a :",a,"b :",b)
+
+# Q8) Addition/ substraction/ Multiplication / Division of Two Variables 
+
+x=10
+y=20
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+
+# Q9) Concatenate Strings
+
+first = "Data"
+second = "Engineer"
+print(first + " "+ second)
+
+# Q10) Convert into a Float Value
+
+x=10
+print(float(x))
