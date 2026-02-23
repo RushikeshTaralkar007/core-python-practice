c8f3d9da92e0192b2354cc225e40d713bdc560f9
Rushikesh Taralkar <rushikeshtaralkar07@gmail.com>
2026-02-23 21:19:28 +0530

    Added Some Python Problem with Solution

--- Basics/01_variable.py
+++ Basics/01_variable.py
@@ -1,3 +1,5 @@
+###################   (Day 1)    ###################
+
 # Q1) Print The Name 
 
 name = "Rushikesh Taralkar"
@@ -55,3 +57,84 @@ print(first + " "+ second)
 
 x=10
 print(float(x))
+
+
+###################   (Day 2)    ###################
+
+# Q11) convert string into int
+
+num = "25"
+print(int(num))
+
+
+# Q12) check Variable type 
+
+x= "Rushi"
+print(type(x))
+
+
+
+## Q13) Take Input From User 
+
+# name = input("Enter Your Full Name :")
+# print("Full Name :", name)
+
+
+
+# # Q14) Add Two User Inputs 
+
+# num1 = int(input("Enter The Num1 :"))
+# num2 = int(input("Enter The Num2 :"))
+# print("Addition :",num1+num2)
+
+
+
+# # Q15) Check if variable is even or odd
+
+# num = int(input("Enter The Number :"))
+# if num % 2 == 0:
+#     print("Even Number")
+# else:
+#     print("Odd Number")
+
+
+# Q16) Find Maximum Of Two Variables 
+
+x=5
+y=8
+print(max(x , y))
+
+# # Q17) Calculate Area Of Rectangle
+
+# length = int(input("Enter The Length :"))
+# width = int(input("Enter The Width :"))
+# area = length * width 
+# print("Area Of Rectangle : ",area)
+
+# # Q18) Calculate Simple Interest 
+
+# Amount = int(input("Enter The Amount :"))
+# Rate = float(input("Enter The Rate :"))
+# Time = float(input("Enter The Year :"))
+# S_I= (Amount * Rate * Time ) / 100
+# print("Simple Interest :", S_I)
+
+
+# # Q19) Check Variable Is Positive , negative or zero
+
+# num = int(input("Enter The Number :"))
+# if num> 0:
+#     print("Number Is Positive")
+# elif num<0:
+#     print("Number Is Negative")
+# else:
+#     print("Number Is Zero")
+
+
+# Q20) string Length 
+
+name = "Rushikesh"
+print(len(name))
+
+
+###################   (Day )    ###################
---
+++ Basics/02_operator.py
@@ -0,0 +1,124 @@
+#############    (Arithmatic Operators)    ##################
+
+###################  ( Day 1 )   ###################
+
+a= 10
+b=5
+
+# Q1) Add Two Numbers 
+
+print(a+b)
+
+# Q2) Substract Two Numbers 
+
+print(a-b)
+
+# Q3) Multipilication of Two Numbers 
+
+print(a*b)
+
+# Q4) Division Of Two Numbers 
+
+print(int(a/b))
+print(a/b)
+
+# Q5) Floor Division
+
+print(a // b)
+
+# Q6) Float Division
+
+print(5.5 // 3)
+
+# Q7) Square Of Number 
+
+x=7
+print(x ** 2)
+
+# Q8) cube of number 
+print(2 **3)
+
+# Q9) Modulus 
+
+print(10 % 2)
+
+# Q10) Power Operator 
+print(4 **3)
+
+# Q11) Combined Operator 
+print(2+3 * 4)
+
+# Q12) Brackets Priority 
+print((2+3) *4)
+
+# Q13) Negative Multiplication
+
+print(-5 * 3)
+
+# Q14) Float Addition 
+
+print(2.5 +3.6)
+
+# Q15) Remainder check even/ odd
+
+num = 8
+print(num % 4)
+
+# Q16) Calculate Area
+
+length = 8
+width = 5
+print("Area:",length*width)
+
+# Q17) BMI Formula 
+
+weight = 60
+height = 1.7
+print(weight / (height ** 2))
+
+# Q18) Add 3 Number
+print(2+3+8)
+
+# Q19) Multiple Subtraction
+
+print(2-5-8)
+
+# Q20) Mixed Division
+
+print(20 // 6)
+
+# Q21) Power With FLoat 
+
+print(4 ** 0.5)
+
+# Q22) Zero Multiplication
+print(10 *0)
+
+# Q23) Modulus Example 
+
+print(25 % 5)
+
+# Q24) Division Result Type 
+
+print(type(10 / 2))
+
+# Q25) Floor Result Type 
+
+print(type(10 // 2))
+
+# Q26) Negative Modulus 
+print(-10 % 3)
+
+# Q27) Exponent Chain 
+
+print(2** 2** 2)
+
+# Q28) Large Multiplication
+
+print(12506 * 2658)
+
+# Q29) Decimal Precision
+print(0.1 + 0.2)
+
+# Q30) Complex Expression 
+print((5+3) * 2+4 -5/2)
