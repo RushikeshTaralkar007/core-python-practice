9d7ad10f04da474f3752b4649d04581c8324716b
Rushikesh Taralkar <rushikeshtaralkar07@gmail.com>
2026-02-28 12:03:01 +0530

    Delete Mini_Project/Card.py

--- Mini_Project/Card.py
+++
@@ -1,7 +0,0 @@
-#########################       🚀 MINI PROJECT 1 (User Input Version)     ##########################
-######################     💳 Smart Credit Card Eligibility & Risk Analyzer     #########################
-
-##########################    ( Used python : Variables , Operators , Conditions )    ####################################
-
-
-
