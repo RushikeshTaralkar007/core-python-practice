0aa87158ed0958f44cb7cd8216613f1b38195a93
Rushikesh Taralkar <rushikeshtaralkar07@gmail.com>
2026-02-28 12:04:12 +0530

    Project 1

---
+++ Mini_Projects/E_commerce.py.py
@@ -0,0 +1,7 @@
+#########################       🚀 MINI PROJECT 1 (User Input Version)     ##########################
+######################     💳 Smart Credit Card Eligibility & Risk Analyzer     #########################
+
+##########################    ( Used python : Variables , Operators , Conditions )    ####################################
+
+
+
