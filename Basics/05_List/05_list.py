3b96bc270ba2ca712b8e576ddbef6291a87a1e4a
Rushikesh Taralkar <rushikeshtaralkar07@gmail.com>
2026-03-20 13:48:05 +0530

    List

---
+++ Basics/05_List/05_list.py
@@ -0,0 +1,172 @@
+###################### (For Data Engineer & Big Data To Solve The List Based Problems)    #########################
+
+
+# # Q1) How To create a List 
+
+# nums = [10,20,30,40]
+# print(nums)
+
+
+
+# # Q2) How To Access List First Element
+
+# nums = [10,20,30,40]
+# print(nums[0])
+
+
+
+# # Q3) How To Access List Last Element
+
+# nums = [10,20,30,40]
+# print(nums[-1])
+
+
+
+# # Q4) How to find the length of the list
+
+# nums = [10,20,30,40]
+# print(len(nums))
+
+
+
+# # Q5) How to Add New Element In the list
+
+# nums = [10,20,30,40]
+# nums.append(50)
+# print(nums)
+
+
+
+# # Q6) How To Insert Element On specific Position
+
+# nums = [10,20,30,40]
+# nums.insert(2,21)
+# print(nums)
+
+
+
+# # Q7) How to Remove The Element 
+
+# nums = [10,20,30,40]
+# nums.remove(20)
+# print(nums)
+
+
+
+# # Q8) How To Delete Last Element
+
+# nums = [10,20,30,40]
+# nums.pop()
+# print(nums)
+
+
+
+# # Q9) check Element Exist In the list
+
+# nums = [10,20,30,40]
+
+# if 20 in nums:
+#     print("Found !")
+
+
+
+
+# # Q10) How to show empty list
+
+# nums = [10,20,30,40]
+# nums.clear()
+# print(nums)
+
+
+
+# # Q11) print All list Elements Using Loop
+
+# nums = [10,20,30,40]
+
+# for n in nums:
+#     print(n)
+
+
+
+# # Q12) Find the Sum Of the list
+
+# nums = [10,20,30,40]
+# total = 0
+
+# for n in nums:
+#     total += n
+# print(total)
+
+
+
+# # Q13) How To Find The Maximunm Element 
+
+# nums = [10,20,30,40]
+
+# max_val = nums[0]
+
+# for n in nums:
+#     if n > max_val:
+#         max_val =n
+# print(max_val)
+
+
+
+# # Q14) Count The Even Numbers 
+
+# nums = [1,2,3,4]
+# count =0 
+
+# for n in nums:
+#     if n % 2 ==0:
+#         count += 1
+# print(count)
+
+
+
+
+# # Q15)Find The Sqaure of the elements in the list 
+
+# nums = [10,20,30,40]
+# squares =[]
+
+# for n in nums:
+#     squares.append(n*n)
+# print(squares)
+
+
+
+# # Q16) Reverse The List
+
+# nums = [10,20,30,40]
+# print(nums[::-1])
+
+
+
+# # Q17) Find The Index Of Element 
+
+# nums = [10,20,30,40]
+# print(nums.index(20))
+
+
+
+# # Q18) Find The count of repeated elements in the list 
+
+# nums = [10,20,20,30,30,40]
+# print(nums.count(20))
+
+
+
+# # Q19) Copy The List 
+
+# nums = [10,20,30,40]
+# copy_list = nums.copy()
+# print(copy_list)
+
+
+
+# # Q20) Sort The List
+
+# nums = [30,40,20,10]
+# nums.sort()
+# print(nums)
