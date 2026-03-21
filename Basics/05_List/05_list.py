fedc7939a1a591828ed785f557b02a0216fcee6c
Rushikesh Taralkar <rushikeshtaralkar07@gmail.com>
2026-03-21 14:11:28 +0530

    List

--- Basics/05_List/05_list.py
+++ Basics/05_List/05_list.py
@@ -170,3 +170,241 @@
 # nums = [30,40,20,10]
 # nums.sort()
 # print(nums)
+
+
+
+
+# # Q21) Remove The Duplicate elements
+
+# nums = [1,2,2,3]
+# unique =[]
+
+# for n in nums:
+#     if n not in unique:
+#         unique.append(n)
+# print(unique)
+
+
+
+
+# # Q22) Remove The Negative Numbers 
+
+# nums = [1,-1,-4,5,9]
+# result =[]
+
+# for n in nums:
+#     if n>=0:
+#         result.append(n)
+# print(result)
+
+
+
+# # Q23) Replace The None Values 
+
+# data = [10,None,30]
+# result =[]
+
+# for v in data:
+#     if v is None:
+#         result.append(0)
+#     else:
+#         result.append(v)
+# print(result)
+
+
+
+
+# # Q24) String Numbers Converted to the integers
+
+# nums = ["1", "2","3","4"]
+# result = []
+
+# for n in nums:
+#     result.append(int(n))
+# print(result)
+
+
+
+# # Q25) Flatten The Nested List
+
+# nested = [[1,2],[3,4]]
+# flat=[]
+
+# for sub in nested:
+#     for item in sub:
+#         flat.append(item)
+# print(flat)
+
+
+
+# # Q26) Find the second largest Number 
+
+# nums = [50,60,80,20,10]
+# nums.sort()
+# print(nums[-1])
+
+
+
+
+# # Q27) Merge The List
+
+# a=[1,2]
+# b=[5,6]
+
+# c=a+b
+# print(c)
+
+
+
+
+# # Q28) Find The List Intersection
+
+# a= [1,2,3]
+# b=[2,3,4]
+
+
+# result=[]
+
+# for n in a:
+#     if n in b:
+#         result.append(n)
+# print(result)
+
+
+
+# # Q29) Check List Is Sorted or not 
+
+# nums = [1,4,3]
+
+# if nums == sorted(nums):
+#     print("Sorted")
+# else:
+#     print("Not Sorted")
+
+
+
+# # Q30) Find The Longest string
+
+# words = ["data","Engineering","ai"]
+# print(max(words,key=len))
+
+
+
+# # Q31) Calculate The Average 
+
+# nums = [10,20,30,40]
+# print(sum(nums)/len(nums))
+
+
+
+# # Q32) Convert numbers into string 
+
+# nums = [10,20,30,40]
+# result = []
+
+# for n in nums:
+#     result.append(str(n))
+# print(result)
+
+
+
+# # Q33) Filter only The String From List
+
+# data = [10,"data",20,"ai"]
+
+# result =[]
+# for item in data:
+#     if isinstance(item, str):
+#         result.append(item)
+# print(result)
+
+
+# # Q34) Double The List Numbers 
+
+# nums = [1,2,3]
+# result =[]
+
+# for n in nums:
+#     result.append(n*2)
+# print(result)
+
+
+
+# # Q35) Find The Top 3 Largest Numbers In the List
+
+# nums=[10,50,60,80,30,90]
+
+# nums.sort(reverse=True)
+
+# print(nums[:-3])
+
+
+
+
+# # Q36) Calculate The Percentage of The List
+
+# nums = [50,100,150]
+
+# total = sum(nums)
+
+# percentage =[]
+
+# for n in nums:
+#     percentage.append((n/total)*100)
+
+# print(percentage)
+
+
+
+
+# # Q37) Can Filter The Greater Then Average Values
+
+# nums = [10,20,30,40]
+
+# avg = sum(nums)/len(nums)
+
+# result =[]
+
+# for n in nums:
+#     if n > avg:
+#         result.append(n)
+# print(result)
+
+
+
+
+# # Q38) Find The sqaures Of The Even elements in the list
+
+# nums =[1,2,3,4,5,6,7,8]
+
+# result =[]
+
+# for n in nums:
+#     if n%2 ==0:
+#         result.append(n*n)
+# print(result)
+
+
+
+# # Q39) Find The Cumulative Sum 
+
+# nums = [1,2,3,4]
+# result =[]
+
+# total =0
+
+# for n in nums:
+#     total +=n
+#     result.append(total)
+
+# print(result)
+
+
+
+
+# # Q40) Find The Index +Value In The List
+
+# nums =[10,20,30]
+
+# for i ,val in enumerate(nums):
+#     print(i,val)
