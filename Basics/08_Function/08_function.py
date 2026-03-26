3139ac9942e3bc5fd347905a32c62bee623871f4
Rushikesh Taralkar <rushikeshtaralkar07@gmail.com>
2026-03-26 12:50:44 +0530

    Function

---
+++ Basics/08_Function/08_function.py
@@ -0,0 +1,88 @@
+####################    (For Data Engineer & Big Data Solve The Python Function Problems)    ###############################
+
+
+# # Q1) How do you define a function in python 
+
+# def greet():
+#     name = input("Enter The Name :")
+#     print("Hello",name)
+
+# greet()
+
+
+
+# # Q2) How do yoy create a fuction with parameters 
+
+# def greet(name):
+#     print("Hello",name)
+# greet("Rushikesh")
+
+
+
+# # Q3) Write a fuction to return the sum of two numbers 
+
+# def add(a,b):
+#     return a+b
+# print(add(5,3))
+
+
+
+# # Q4) Write a fuction to calculate the sqaure of a number
+
+# def square(n):
+#     return n*n
+# print(square(4))
+
+
+
+# # Q5) Write a function to check if a number is even 
+
+# def is_even(n):
+#     return n%2 ==0
+# print(is_even(10))
+
+
+
+# # Q6) write a fuction to calculate factorial 
+
+# def factorial(n):
+#     fact =1
+#     for i in range(1,n+1):
+#         fact *=i
+#     return fact 
+
+# print(factorial(5))
+
+
+
+# # Q7) Write a fuction to return the maximum of two numbers 
+
+# def maximum(a,b):
+#     if a>b:
+#         return a
+#     else:
+#         return b
+# print(maximum(10,5))
+
+
+# # Q8) write a fuctuion to return the length of a string 
+
+# def string_length(s):
+#     return len(s)
+# print(string_length("python"))
+
+
+
+# # Q9) write a fuction that converts a string to uppercase 
+
+# def to_upper(text):
+#     return text.upper()
+# print(to_upper("data engineer"))
+
+
+# # Q10) write a fuction that prints numbers from 1 to N
+
+# def print_numbers(n):
+#     for i in range(1,n+1):
+#         print(i)
+# print_numbers(5)
