0c6c7675d04dbbdd5498f6cbf3139af63d788cf0
Rushikesh Taralkar <rushikeshtaralkar07@gmail.com>
2026-03-28 20:40:14 +0530

    Function

--- Basics/08_Function/08_function.py
+++ Basics/08_Function/08_function.py
@@ -189,3 +189,167 @@
 
 
 
+
+# # Q21) Wwrite a fuction to calculate average 
+
+# def average(lst):
+#     return sum(lst) / len(lst)
+
+# print(average([10,20,30,40]))
+
+
+
+
+# # Q22) Write a recursive factorial fuction 
+
+# def factorial(n):
+#     if n==1:
+#         return 1
+#     return n * factorial(n-1)
+
+# print(factorial(5))
+
+
+
+# # Q23) Write a fuction to remove duplicates from a list 
+
+# def remove_duplicates(lst):
+#     return list(set(lst))
+
+# print(remove_duplicates([1,2,2,3,3]))
+
+
+
+# # Q24) Write a fuction to check prime number 
+
+# def is_prime(n):
+#     for i in range(2,n):
+#         if n%i == 0:
+#             return False
+#     return True
+
+# print(is_prime(7))
+
+
+
+# # Q25) Write a fuction to generate Fibonacci series 
+
+# def fibonacci(n):
+#     a,b = 0,1
+
+#     for i in range(n):
+#         print(a)
+#         a,b = b,a+b
+# fibonacci(5)
+
+
+
+
+# # Q26) Write a fuction to count words in a sentence 
+
+# def count_words(s):
+#     return len(s.split())
+
+# print(count_words("Python Is Powerful"))
+
+
+
+
+# # Q27) Write a fuction to find minimum in list
+
+# def minimum(lst):
+#     return min(lst)
+
+# print(minimum([3,0,2,8,6,9]))
+
+
+
+# # Q28) Write a fuction to sort a list 
+
+# def sort_list(lst):
+#     lst.sort()
+#     return lst
+
+# print(sort_list([5,6,1,2]))
+
+
+
+
+# # Q29) Write a fuction to check if list is empty 
+
+# def is_empty(lst):
+#     return len(lst) == 0
+
+# print(is_empty([]))
+
+
+
+# # Q30) Write a fuction to convert list to dictionary 
+
+# def list_to_dict(keys,values):
+#     return dict(zip(keys,values))
+
+# print(list_to_dict(["a","b"],[1,3]))
+
+
+
+# # Q31) Write a fuction to clean whitespace from string 
+
+# def clean_txt(text):
+#     return(text.strip())
+
+# print(clean_txt(" Data "))
+
+
+
+# # Q32) Write a fuction to convert dictionary values to integers 
+
+# def convert_int(d):
+#     return {k:int(v) for k,v in d.items()}
+
+# print(convert_int({"a":'10',"b":'20'}))
+
+
+
+# # Q33) Write a fuction to calculate total sales from records 
+
+# def total_sales(records):
+#     total =0
+
+#     for r in records:
+#         total += r["Sales"]
+
+#     return total 
+
+# data =[{"Sales":100},{"Sales":200}]
+
+# print(total_sales(data))
+
+
+
+
+# # Q34) Write a fuction to filter records with sales > 100
+
+
+# def filter_sales(data):
+#     result =[]
+    
+
+#     for d in data:
+#         if d["sales"] >100:
+#             result.append(d)
+#     return result
+
+
+
+# # Q35) Write a fuction to count frequency of elements 
+
+# def frequency(lst):
+#     freq ={}
+
+#     for i in lst:
+#         freq[i] = freq.get(i,0) + 1
+
+#     return freq
+
+# print(frequency([1,1,2,3]))
