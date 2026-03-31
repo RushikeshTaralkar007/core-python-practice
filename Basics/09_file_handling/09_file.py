41711ed8274036028610c1352d51fcb2a6140084
Rushikesh Taralkar <rushikeshtaralkar07@gmail.com>
2026-03-31 09:42:23 +0530

    File Handling

---
+++ Basics/09_file_handling/09_file.py
@@ -0,0 +1,62 @@
+####################    (For Data Engineer & Big Data Solve The Python File Handling Problems)    ###############################
+
+
+### Syntax
+
+# file = open("File_Name", "mode")
+
+
+## Q1) Read The Entire Data 
+
+# file = open("C:\Desktop\Python\Git_Core_Python\core-python-practice\example.txt","r")
+
+# const = file.read()
+# print(const)
+# file.close 
+
+
+
+## Q2) Read First Line 
+
+# file = open("C:\Desktop\Python\Git_Core_Python\core-python-practice\example.txt",'r')
+
+# content = file.readline()
+
+# print(content)
+# file.close()
+
+
+# # Q3) Read the entire data in List format 
+
+# file = open("C:\Desktop\Python\Git_Core_Python\core-python-practice\example.txt",'r')
+
+# content = file.readlines()
+# print(content)
+# file.close()
+
+
+# # Q4) Write to a file 
+
+# file = open('example2.txt','w') ## Create a new file 
+
+
+# # Q5) write a txt on the new file 
+
+# file = open('example2.txt','w')
+# file.write("Hello, Rushi")
+# file.close()
+
+
+# ## Q6) Used Append Mode
+
+file = open("C:\Desktop\Python\Git_Core_Python\example2.txt",'a')
+
+file.write("\n,How Are you ?")
+file.close()
+ 
+
+
+
+
+
+
