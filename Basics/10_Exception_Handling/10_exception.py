f2a59e9b6f7458536621d9d8d54d4dd4f3b80ec7
Rushikesh Taralkar <rushikeshtaralkar07@gmail.com>
2026-04-04 07:34:59 +0530

    Exception Handling

--- Basics/10_Exception_Handling/10_exception.py
+++ Basics/10_Exception_Handling/10_exception.py
@@ -138,3 +138,219 @@ print("Raw Data :",data)
 #     print("Value Error")
 # except ZeroDivisionError:
 #     print("Division Error")
+
+
+
+# # Q11) Raise Custom Error 
+
+# x = -5
+# if x<0:
+#     raise ValueError("Negative Not Allowed")
+
+
+
+# # Q12) Skip Negative Numbers 
+
+# for i in data:
+#     try:
+#         num = int(i)
+#         if num < 0:
+#             raise ValueError
+#         print(num)
+#     except:
+#         print("Skipped :",i)
+
+
+
+# # Q13) Count Valid Numbers 
+
+# count =0
+# for i in data:
+#     try:
+#         int(i)
+#         count += 1
+#     except:
+#         pass
+# print(count)
+
+
+
+# # Q14) Count Invalid Numbers 
+
+# count =0 
+# for i in data:
+#     try:
+#         int(i)
+#     except:
+#         count +=1
+# print(count)
+
+
+
+# # Q15) Replace Invalid with 0 
+
+# clean =[]
+# for i in data:
+#     try:
+#         clean.append(int(i))
+#     except:
+#         clean.append(0)
+# print(clean)
+
+
+
+# # Q16) Sum Valid Numbers 
+
+# total =0
+
+# for i in data:
+#     try:
+#         total += int(i)
+#     except:
+#         pass
+
+# print(total)
+
+
+
+# # Q17) Average Of Invalid Number
+
+# nums =[]
+# for i in data:
+#     try:
+#         nums.append(int(i))
+#     except:
+#         pass
+# print(sum(nums)/len(nums))
+
+
+# # Q18) Find Maximum
+
+# nums =[]
+# for i in data:
+#     try:
+#         nums.append(int(i))
+#     except:
+#         pass
+# print(max(nums))
+
+
+
+# # Q19) Find Minimum 
+
+# nums =[]
+# for i in data:
+#     try:
+#         nums.append(int(i))
+#     except:
+#         pass
+# print(min(nums))
+
+
+
+# # Q20) Handle Empty List Safely 
+
+# nums=[]
+# try:
+#     print(sum(nums)/len(nums))
+# except ZeroDivisionError:
+#     print("No Data")
+
+
+
+# # Q21) Convert And square Numbers 
+
+# for i in data:
+#     try:
+#         print(int(i)**2)
+#     except ValueError:
+#         print("Error")
+
+
+
+# # Q22) Filter Positive Numbers 
+
+# for i in data:
+#     try:
+#         num = int(i)
+#         if num >0:
+#             print(num)
+#     except ValueError:
+#         pass
+
+
+
+# # Q23) Handle File Not Found 
+
+# try:
+#     open("wrong.txt")
+# except FileNotFoundError:
+#     print("File Missing")
+
+
+# # Q24) Nested Try 
+
+# try:
+#     try:
+#         x= int('abc')
+#     except:
+#         print("Inner")
+# except:
+#     print("Outer")
+
+
+
+# # Q25) Use Pass To ignore 
+
+# try:
+#     int('abc')
+# except:
+#     pass
+
+
+# # Q26) Multiple exception in one line 
+
+# try:
+#     int('abc')
+# except (TypeError,ValueError, ZeroDivisionError):
+#     print("Error")
+
+
+# # Q27) Custom Exception 
+
+# class MyError(Exception):
+#     pass
+
+
+# # Q28) Raise Custom Exception 
+
+# raise MemoryError("Custom")
+
+
+# Q29) Logging Error 
+
+# try:
+#     int('abc')
+# except Exception as e:
+#     print("Logged :",e)
+
+
+# # Q30) Safe Devision 
+
+# def safe_div(a,b):
+#     try:
+#         return a/b
+#     except ZeroDivisionError:
+#         return "Error"
+# print(safe_div(10,0))
+
+
+# # Q31) Exception In Function 
+
+# def convert(x):
+#     try:
+#         return int(x)
+#     except:
+#         return None
+
+
